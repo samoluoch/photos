4b31af3907df6cd733432a19b3d4a644a5c1fa4c
Sam Oluoch <samsoluoch@gmail.com>
2018-09-26 20:34:10 +0300

    registered the app in the settings, created views function for the index page, created welcome.html, registered the index url in the urls.py, and rendered information to the welcome.html template

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
