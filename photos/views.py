e473c70f1a7c53bd04331a17ac4340325c274641
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 23:18:59 +0300

    created thumbnail class that holds each image rendered to the templates after the function loops through image in images. Displayed more image details to the image.html template that allows users to view additional information after they click on an image

--- photos/views.py
+++ photos/views.py
@@ -39,7 +39,7 @@ def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = Image.search_by_title(search_term)
+        searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
 
         return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
