f387d750efd7421d2cd52e1fad9ba06477bdf6c0
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 08:21:22 +0300

    created a category class

--- photos/models.py
+++ photos/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 
 # Create your models here.
+
+class Category(models.Model):
+    '''
+    This is the cetgories class
+    '''
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
 class Image(models.Model):
     '''
     This is image class model
@@ -10,7 +21,7 @@ class Image(models.Model):
     name = models.CharField(max_length =60)
     description = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    # category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category)
     # location = models.ForeignKey(Location)
 
     def save_image(self):
@@ -25,6 +36,8 @@ class Image(models.Model):
     def search_by_location(cls, search_term):
         image = cls.objects.filter(title__icontains=search_term)
         return image
+
+
 
 
 
