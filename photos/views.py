448254cee69860e3c5f4d4ba05f2a8742c4e35c9
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 07:51:42 +0300

    created a template for displaying the images description and other image details

--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse,Http404
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+# def image(request,image_id):
+#     try:
+#         image = Image.objects.get(id = image_id)
+#     except DoesNotExist:
+#         raise Http404()
+#     return render(request,"description.html", {"image":image})
