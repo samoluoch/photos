b7396a9ef39a4479e66c64929edfcbb58c76f544
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 09:39:54 +0300

    defined a function that displays images based on the date posted

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .models import Image
+
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+
+# FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+def image_of_day(request):
+    date = dt.date.today()
+    news = Image.todays_image()
+    return render(request, 'all-images/image.html', {"date": date, "news": news})
 
 
 def image(request,image_id):
@@ -11,4 +22,4 @@ def image(request,image_id):
         image = Image.objects.get(id = image_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"image.html", {"image":image})
+    return render(request,"all-images/image.html", {"image":image})
