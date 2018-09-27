471ea51eb87a0d0f304350eca8896264a3fc7deb
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 08:13:11 +0300

    created functions for saving the images and classmethod for searching the image by category

--- photos/models.py
+++ photos/models.py
@@ -3,12 +3,24 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
+    '''
+    This is image class model
+    '''
     image = models.ImageField(upload_to='image/', null=True)
     name = models.CharField(max_length =60)
     description = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     # category = models.ForeignKey(Category)
     # location = models.ForeignKey(Location)
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        image = cls.objects.filter(title__icontains=search_term)
+        return image
+
 
 
 
