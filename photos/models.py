1038736c6621cd964417ed3fca2d80395b63b890
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 08:06:03 +0300

    created the image model

--- photos/models.py
+++ photos/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='image/', null=True)
+    name = models.CharField(max_length =60)
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    # category = models.ForeignKey(Category)
+    # location = models.ForeignKey(Location)
+
+
+
