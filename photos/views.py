6e757a5249006cfae179c9c2e514a2b549d7beb7
Sam Oluoch <samsoluoch@gmail.com>
2018-09-27 09:30:04 +0300

    created views function for displaying image details. Registered the template url in the urls.py

--- photos/views.py
+++ photos/views.py
@@ -6,9 +6,9 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 
-# def image(request,image_id):
-#     try:
-#         image = Image.objects.get(id = image_id)
-#     except DoesNotExist:
-#         raise Http404()
-#     return render(request,"image.html", {"image":image})
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"image.html", {"image":image})
